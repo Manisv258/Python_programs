01a4bd2f463f37e9ff148c6bbbcdbfff3cb1b892
Manivannan <manivannan.sv258@gmail.com>
2025-05-27 14:00:36 +0530

    changes in decorator program

--- Decorators.py
+++ Decorators.py
@@ -2,15 +2,16 @@
    Also decorator is a function which takes another function as argument and returns new function(wrapper).
 """
 def decorator(func):
-    def wrapper():
-        print("Before calling the function")
-        func()
-        print("After calling the function")
+    def wrapper(*args,**kwargs):
+        print(f"The function name is {func.__name__} and args is {args} and kwargs is {kwargs}")
+        result = func(*args,**kwargs)
+        print(f"The result is {result}")
+        return result
     return wrapper
 
 @decorator
 
-def greet():
-    print("Hello Mani")
+def addition(a,b,c):
+    return a + b + c
 
-greet()
+addition(10,20,c=30)
